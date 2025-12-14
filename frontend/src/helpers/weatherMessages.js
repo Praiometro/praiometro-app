e1e10ec085ec64e758ed45bf5a8b71b0f5857144
Disklo <rafaellcarvalho5@gmail.com>
2025-12-14 19:38:07 -0300

    Corrigidos erros gramaticais

--- frontend/src/helpers/weatherMessages.js
+++ frontend/src/helpers/weatherMessages.js
@@ -1,41 +1,41 @@
 
 function getHumidityMessage(humidity) {
     if (humidity >= 80) {
-        return "Umidade alta, vista roupas leves!";
+        return "Umidade alta. Vista roupas leves!";
     } else if (humidity >= 60) {
         return "Sem riscos, mas não deixe de se hidratar!";
     } else if (humidity >= 40) {
-        return "Risco moderado, hidrate-se!";
+        return "Risco moderado. Hidrate-se!";
     } else if (humidity >= 20) {
-        return "Risco alto, hidrate-se e fique em locais frescos!";
+        return "Risco alto. Hidrate-se e fique em locais frescos!";
     } else {
-        return "Risco extremo, hidrate-se ao máximo!";
+        return "Risco extremo. Hidrate-se ao máximo!";
     }
 }
 
 function getSensationMessage(temperature) {
     if (temperature >= 36) {
-        return "Calor extremo, fique em locais frescos e ventilados!";
+        return "Calor extremo. Fique em locais frescos e ventilados!";
     } else if (temperature >= 26) {
-        return "Calor, hidrate-se e use protetor solar!";
+        return "Calor. Hidrate-se e use protetor solar!";
     } else if (temperature >= 16) {
-        return "Clima agradável, aproveite!";
+        return "Clima agradável. Aproveite!";
     } else {
-        return "Frio, use roupas quentes e proteja-se do vento!";
+        return "Frio. Use roupas quentes e proteja-se do vento!";
     }
 }
 
 function getUVIndexMessage(uvIndex) {
     if (uvIndex <= 2) {
-        return "Risco baixo, use protetor solar se ficar exposto por muito tempo.";
+        return "Risco baixo. Use protetor solar se ficar exposto por muito tempo.";
     } else if (uvIndex <= 5) {
-        return "Risco moderado, use protetor solar e evite exposição direta ao sol.";
+        return "Risco moderado. Use protetor solar e evite exposição direta ao sol.";
     } else if (uvIndex <= 7) {
-        return "Risco alto, use protetor solar e evite exposição direta ao sol durante o pico do dia.";
+        return "Risco alto. Use protetor solar e evite exposição direta ao sol durante o pico do dia.";
     } else if (uvIndex <= 10) {
-        return "Risco muito alto, use roupas protetoras e evite exposição direta ao sol.";
+        return "Risco muito alto. Use roupas protetoras e evite exposição direta ao sol.";
     } else {
-        return "Risco extremo, evite exposição direta ao sol e use roupas protetoras.";
+        return "Risco extremo. Evite exposição direta ao sol e use roupas protetoras.";
     }
 }
 
